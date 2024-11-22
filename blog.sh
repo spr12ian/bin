b914845d6738d36c25de6d9ad32633c0cd1e34a8
spr12ian <173560+spr12ian@users.noreply.github.com>
2024-11-22 22:19:42 +0000

    Fixed cd error

--- blog.sh
+++ blog.sh
@@ -2,7 +2,7 @@
 
 focus-here.sh
 
-cd ~/${GITHUB_PARENT}/spr12ian.github.io
+cd ${GITHUB_PARENT}/spr12ian.github.io  || { echo "ERROR: ${GITHUB_PARENT}/spr12ian.github.io not found"; exit 1; }
 
 new_blog="hugo new blog/`date -I`.md"
 
