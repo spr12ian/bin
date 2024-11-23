0645077defba8aec2de191cbd46ad4a54a4088f1
spr12ian <173560+spr12ian@users.noreply.github.com>
2024-11-23 20:54:09 +0000

    install-nvm.sh does not work as expected

--- install-nvm.sh
+++ install-nvm.sh
@@ -1,1 +1,38 @@
 #!/bin/bash
+
+retval=$(command -v nvm); echo "retval: ${retval}"
+exit
+#trap 'echo "Executing: $BASH_COMMAND"' DEBUG
+# Check if nvm is installed
+if command -v nvm &> /dev/null; then
+    echo "nvm is installed. Version: $(nvm --version)"
+else
+    echo "nvm is not installed"
+fi
+
+exit
+
+# Check if git is installed
+if command -v nvm &> /dev/null; then
+    echo "nvm is installed. Version: $(nvm --version)"
+else
+    # Fetch the latest release tag from the nvm GitHub repository
+    latest_version=$(curl -s https://api.github.com/repos/nvm-sh/nvm/releases/latest | grep '"tag_name"' | sed -E 's/.*"v([^"]+)".*/\1/')
+
+    # Check if the version was retrieved successfully
+    if [[ -n "$latest_version" ]]; then
+        echo "The latest version of nvm is: $latest_version"
+    else
+        echo "Failed to retrieve the latest nvm version."
+        exit 1
+    fi
+
+    # Download and install the latest version
+    echo "Installing nvm version $latest_version..."
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v"$latest_version"/install.sh 2>/dev/null | bash
+
+    # Verify the installation
+    echo "Verifying nvm installation..."
+    source ~/.bashrc
+    nvm --version
+fi
--- update-linux.sh
+++ update-linux.sh
@@ -5,7 +5,6 @@ sudo apt upgrade -y
 
 install-linux-packages.sh
 
-sudo apt autoremove -y
 sudo apt --purge autoremove -y
 
 cat /etc/os-release
