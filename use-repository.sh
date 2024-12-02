915adc8ecd697d4cacfc50f9f3a299a473314eb0
spr12ian <173560+spr12ian@users.noreply.github.com>
2024-12-02 18:04:26 +0000

    use repository

--- use-repository.sh
+++ use-repository.sh
@@ -1,1 +1,26 @@
 #!/bin/bash
+
+if [ $# -gt 1 ]; then
+    echo "Too many parameters!"
+    exit
+fi
+
+if [ $# -eq 0 ]; then     # if no arguments provided, prompt user
+    echo "What repository should I use?"
+    read -r repo
+fi
+
+if [ $# -eq 1 ]; then
+    repo=$1
+fi
+
+repoDir="${GITHUB_PARENT}/${repo}" 
+
+cd "${repoDir}" || {
+    echo "ERROR: cd ${repoDir} failed"
+    exit
+}
+
+./setup-development-environment.sh
+
+code .
