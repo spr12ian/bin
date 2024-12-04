15569522a88973b52427d74b50b472704f39788f
spr12ian <173560+spr12ian@users.noreply.github.com>
2024-12-04 08:49:05 +0000

    Update venv install

--- install-python.sh
+++ install-python.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-sudo apt install -y python3 python3-pip python3.10-venv
+sudo apt install -y python3 python3-pip
+
+# Determine the Python 3 version
+python_full_version=$(python3 --version 2>&1 | awk '{print $2}')
+python_version="$(echo $python_full_version | cut -d. -f1-2)"
+echo "Python version: $(echo $python_version | cut -d. -f1-2)"
+
+# Check if `venv` is available
+if ! command -v venv &> /dev/null; then
+    # Install `python3-venv` package for Debian/Ubuntu
+    if [[ $(grep -E '^ID=' /etc/os-release | cut -d= -f2) =~ ^(ubuntu|debian)$ ]]; then
+        sudo apt install -y python${python_version}-venv
+    # For other systems, you might need to use pip or other package managers
+    else
+        echo "Please install 'venv' using your system's package manager."
+        exit 1
+    fi
+fi
