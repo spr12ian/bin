9fd4775fc1fcac2aa56f49cba13ce25d5ac765e4
spr12ian <173560+spr12ian@users.noreply.github.com>
2024-12-04 12:53:34 +0000

    Python with logging (pwl.sh)

--- install-python-package.sh
+++ install-python-package.sh
@@ -1,1 +1,30 @@
 #!/bin/bash
+
+if [ $# -gt 1 ]; then
+    echo "Too many parameters!"
+    exit
+fi
+
+if [ $# -eq 0 ]; then # if no arguments provided, prompt user
+    echo "What package should I add?"
+    read -r package
+fi
+
+if [ $# -eq 1 ]; then
+    package=$1
+fi
+
+log_file="install-pip-${package}.log"
+error_log_file="install-pip-${package}-error.log"
+
+echo "Installing ${package}"
+python3 -m pip install "${package}" >"${log_file}" 2>"${error_log_file}"
+
+# Check if the file exists and its size is zero
+if [ -f "$log_file" ] && [ ! -s "$log_file" ]; then
+    rm "$log_file"
+fi
+
+if [ -f "$error_log_file" ] && [ ! -s "$error_log_file" ]; then
+    rm "$error_log_file"
+fi
--- pwl.sh
+++ pwl.sh
@@ -1,1 +1,24 @@
 #!/bin/bash
+
+# pwl == Python with logging (don't use .py extension)
+
+if [ $# -eq 0 ]; then
+    echo "Python file required"
+    exit 1
+fi
+
+args="$*"
+python_file=$1
+log_file="${python_file}.log"
+error_log_file="${python_file}_error.log"
+
+python3 -m "${args}" >"${log_file}" 2>"${error_log_file}"
+
+# Check if the file exists and its size is zero
+if [ -f "$log_file" ] && [ ! -s "$log_file" ]; then
+    rm "$log_file"
+fi
+
+if [ -f "$error_log_file" ] && [ ! -s "$error_log_file" ]; then
+    rm "$error_log_file"
+fi
