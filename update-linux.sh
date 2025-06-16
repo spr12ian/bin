e5aef8ea8d66c241fc2a608da7244b1a30565ec2
spr12ian <173560+spr12ian@users.noreply.github.com>
2025-06-16 08:56:39 +0100

    reload ssh on update

--- update-linux.sh
+++ update-linux.sh
@@ -10,5 +10,8 @@ sudo apt --purge autoremove -y
 sudo locale-gen en_GB.UTF-8
 sudo update-locale LANG=en_GB.UTF-8
 
+sudo systemctl daemon-reexec
+sudo systemctl daemon-reload
+
 cat /etc/os-release
 hostnamectl
