a0d7da9e5c70eba5417ef9bf94b7f380942168bb
spr12ian <173560+spr12ian@users.noreply.github.com>
2025-06-19 09:07:52 +0100

    symlinks looks complete

---
+++ cleanup-symbolic-links.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+original_dir="${GITHUB_PARENT}/bin"
+target_dir="$HOME/.local/bin"
+
+# Check original directory
+if [ ! -d "${original_dir}" ]; then
+  echo "Original directory does not exist: ${original_dir}"
+  exit 1
+fi
+
+# Check target directory exists
+if [ ! -d "${target_dir}" ]; then
+  echo "Target directory does not_exist: ${target_dir}"
+  exit 1
+fi
+
+find "$target_dir" -type l | while read -r symlink; do
+    target=$(readlink -f "$symlink")
+    if [[ "$target" == "$original_dir"* ]]; then
+        echo "Removing symlink: $symlink -> $target"
+        rm "$symlink"
+    fi
+done
+
+ls -al "$target_dir"
