427d865c6f1a3cf3f2a2b10a07bedc8b24f0357a
spr12ian <173560+spr12ian@users.noreply.github.com>
2025-06-21 07:42:50 +0100

    wrapper for function in .post_bashrc

--- about.sh
+++ about.sh
@@ -1,23 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-clear
-echo $#
-if [ $# -eq 0 ]; then     # if no arguments provided, prompt user
-    echo -n "What commands are you asking about?> "
-    read -r args
-else
-    args=$*
-fi
-
-for cmd in $args  # for each command entered
-do
-    echo "$cmd"
-    echo -n "executable: "
-    which "$cmd"
-    echo -n "all files: "
-    whereis "$cmd"
-    echo "functions:"
-    whatis "$cmd"
-    echo "===================================================================="
-done
+about "$@"
