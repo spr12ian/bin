a846209e86eae588cc9b7d4586960a5ffe8ac215
spr12ian <173560+spr12ian@users.noreply.github.com>
2025-07-01 10:47:07 +0100

    Stick to version 14 of asdf

--- install_asdf.sh
+++ install_asdf.sh
@@ -2,17 +2,21 @@
 set -euo pipefail
 
 if [ -d "$HOME/.asdf" ]; then
-  git -C ~/.asdf pull
+  echo "✅ Already installed: asdf..."
 else
-  git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.18.0
+  echo "✅ Installing asdf..."
+  # After v0.14.0 it gets complicated
+  git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.0
 fi
 
 asdf --version
 
 # Import Node.js release team's GPG keys (essential step)
+echo "✅ Importing GPG keys..."
 bash ~/.asdf/plugins/nodejs/bin/import-release-team-keyring
 
 # Then install Node.js
+echo "✅ Installing Node.js..."
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 asdf install nodejs latest
 asdf global nodejs latest
