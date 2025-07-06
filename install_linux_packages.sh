7f3e8526093ec88cecf780e7c8448968a3ebde30
spr12ian <173560+spr12ian@users.noreply.github.com>
2025-07-06 14:35:57 +0100

    graphviz

--- install_linux_packages.sh
+++ install_linux_packages.sh
@@ -8,6 +8,7 @@ setup_symbolic_links   # Create symbolic links for these commands to work
                        # Until they are all functions
 
 install_gh                        # GitHub CLI for managing GitHub repositories
+install_apt_package graphviz      # Graphviz
 install_apt_package jq            # Command_line JSON processor
 install_apt_package moreutils     # additional Unix utilities (e.g. sponge)
 install_asdf                      # Version Manager & LTS node.js
