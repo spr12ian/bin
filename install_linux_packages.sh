3375ad91a8c7078a3e10b0318af154e925c7a84a
spr12ian <173560+spr12ian@users.noreply.github.com>
2025-07-22 11:00:08 +0100

    typescript

--- install_linux_packages.sh
+++ install_linux_packages.sh
@@ -7,20 +7,21 @@ source "$SYMLINKS_SOURCE_DIR/bash_functions"
 setup_symbolic_links   # Create symbolic links for these commands to work
                        # Until they are all functions
 
-install_gh                        # GitHub CLI for managing GitHub repositories
-install_apt_package age           # Secure file encryption
-install_apt_package graphviz      # Graphviz
-install_apt_package jq            # Command_line JSON processor
-install_apt_package moreutils     # Additional Unix utilities (e.g. sponge)
-install_asdf                      # Version Manager & LTS node.js
-install_npm                       # Version Manager & LTS node.js
-install_openssh_server            # SSH server for remote access
-install_pipx                      # Install & run Python apps in isolated environments
-install_apt_package poppler-utils # PDF utilities
-install_python                    # Python programming language
-install_rust                      # Rust programming language
-install_apt_package shellcheck    # Shell script analysis tool
-install_snap_packages             # Snap package manager
-install_apt_package sqlite3       # Sql command line
-install_apt_package tesseract_ocr # Optical Character Recognition
-install_vs_code                   # Visual Studio Code for code editing
+install_apt_package age             # Secure file encryption
+install_apt_package graphviz        # Graphviz
+install_apt_package jq              # Command_line JSON processor
+install_apt_package moreutils       # Additional Unix utilities (e.g. sponge)
+install_apt_package node-typescript # Additional Unix utilities (e.g. sponge)
+install_apt_package poppler-utils   # PDF utilities
+install_apt_package shellcheck      # Shell script analysis tool
+install_apt_package sqlite3         # Sql command line
+install_apt_package tesseract_ocr   # Optical Character Recognition
+install_asdf                        # Version Manager & LTS node.js
+install_gh                          # GitHub CLI for managing GitHub repositories
+install_npm                         # Version Manager & LTS node.js
+install_openssh_server              # SSH server for remote access
+install_pipx                        # Install & run Python apps in isolated environments
+install_python                      # Python programming language
+install_rust                        # Rust programming language
+install_snap_packages               # Snap package manager
+install_vs_code                     # Visual Studio Code for code editing
